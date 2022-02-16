da5cbb8574f42ba8d1360faf9b8ed01d959c5d14
unknown <ehiaguinaosesojie@gmail.com>
2022-02-16 18:17:33 +0100

    innit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -81,9 +81,11 @@ class Contact extends Component {
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {name}<br />
+                     {email}<br />
 						   {street} <br />
 						   {city}, {state} {zip}<br />
 						   <span>{phone}</span>
+                     
 					   </p>
 				   </div>
 
